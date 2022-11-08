6ef48421c6a129059e10643044aab3d4387e87d0
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-11-08 12:00:09 +0530

    Last Sync: 2022-11-08 12:00:09

--- C/TermWork/4.c
+++ C/TermWork/4.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
+
+char marks_to_grade(float mark) {
+  if (mark < 30) {
+    return 'F';
+  } else if (mark < 45) {
+    return 'E';
+  } else if (mark < 60) {
+    return 'D';
+  } else if (mark < 75) {
+    return 'C';
+  } else if (mark < 85) {
+    return 'B';
+  } else
+    return 'A';
+}
+
 void main() {
-  int marks;
-  printf("Enter your marks:");
-  scanf("%d", &marks);
-  if (marks < 0 || marks > 100) {
-    printf("Out of range marks.");
-  } else if (marks < 30) {
-    printf("F");
-  } else if (marks < 45) {
-    printf("E");
-  } else if (marks < 60) {
-    printf("D");
-  } else if (marks < 75) {
-    printf("C");
-  } else if (marks < 85) {
-    printf("B");
-  } else printf("A");
+  float marks[5];
+  float sum = 0;
+  printf("Enter your marks in 5 subjects:\n");
+  int i = 0;
+  while (i < 5) {
+    scanf("%f", &marks[i]);
+    if (marks[i] < 0 || 100 < marks[i]) {
+      printf("Out of range marks.\nPlease enter correct marks.\n");
+      continue;
+    }
+    printf("Your marks in this subject is: %c\n", marks_to_grade(marks[i]));
+    sum += marks[i];
+    i++;
+  }
+
+  printf("Your total percentage is :\n%.2f\nYou final grade is:\n%c", sum / 5,
+         marks_to_grade(sum / 5));
 }
