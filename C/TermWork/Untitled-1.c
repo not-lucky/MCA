ac3b149660624d1b4e647b2927188649d042e736
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-11-10 18:00:10 +0530

    Last Sync: 2022-11-10 18:00:10

--- C/TermWork/Untitled-1.c
+++ C/TermWork/Untitled-1.c
@@ -8,25 +8,11 @@ int main() {
       printf("  ");
     }
     for (int j = 1; j <= i; j++) {
-      printf("%d ", j);
+      printf("%c ", 'A' + n - j);
     }
     for (int j = i - 1; j >= 1; j--) {
-      printf("%d ", j);
+      printf("%c ", 'A' + n - j);
     }
-
-    printf("\n");
-  }
-  for (int i = n - 1; i >= 1; i--) {
-    for (int j = 1; j <= n - i; j++) {
-      printf("  ");
-    }
-    for (int j = 1; j <= i; j++) {
-      printf("%d ", j);
-    }
-    for (int j = i - 1; j >= 1; j--) {
-      printf("%d ", j);
-    }
-
     printf("\n");
   }
 }
