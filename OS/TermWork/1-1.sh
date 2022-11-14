5749e55cbb1134b0448ed33340d95d0c7474f5e5
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-11-14 06:19:26 +0530

    ad

--- OS/TermWork/1-1.sh
+++ OS/TermWork/1-1.sh
@@ -1,14 +1,13 @@
-echo "This program will calculate the Net Salary of employee and also amount credited to the employee in first month."
+echo "This program will calculate the Net Salary of employee and also amount credited to the employee in first month"
 basic_salary=20000
-DA_basic=$(echo $basic_salary*0.4 | bc -l)
-HRA_basic=$(echo $basic_salary*0.1 | bc -l)
+DA_basic=$(echo "$basic_salary * 0.4" | bc -l)
+HRA_basic=$(echo "$basic_salary * 0.1" | bc -l)
 TA_basic=2000
-retirement_ins_fund=$(echo $basic_salary*0.02 | bc -l)
+retirement_ins_fund=$(echo "$basic_salary * 0.02" | bc -l)
 one_time_payout_furniture=75000
 one_time_payout_laptop=45000
 
-Net_salary=$(echo $basic_salary + $DA_basic + $HRA_basic + $TA_basic - retirement_ins_fund | bc -l)
-
-Total_salary_for_first_month=$(echo $Net_salary + $one_time_payout_furniture + $one_time_payout_laptop | bc)
+Net_salary=$(echo "$basic_salary + $DA_basic + $HRA_basic + $TA_basic - $retirement_ins_fund" | bc -l)
+Total_salary_for_first_month=$(echo "$Net_salary + $one_time_payout_furniture + $one_time_payout_laptop"| bc -l)
 echo "Net salary of the employee is : "$Net_salary
 echo "Total salary of the employee for the first month : "$Total_salary_for_first_month
