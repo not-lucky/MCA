d71d1aa1f78f65872750dc3fa67f8dbe3a77746b
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-11-16 06:00:08 +0530

    Last Sync: 2022-11-16 06:00:08

--- C/TermWork/a.sh
+++ C/TermWork/a.sh
@@ -1,3 +1,23 @@
-for (( i = 1; i <= 12; i++ )); do
-	$(touch ${i}.c)
+echo "enter the number"
+read n
+echo "enter number in an array"
+for ((i = 0; i < n; i++)); do
+	read arr[$i]
 done
+#logic for selection sort
+for ((i = 0; i < n - 1; i++)); do
+	small=${arr[$i]}
+	index=$i
+	for ((j = i + 1; j < n; j++)); do
+		if ((arr[j] <= small)); then
+			small=${arr[$j]}
+			index=$j
+		fi
+	done
+
+	temp=${arr[$i]}
+	arr[$i]=$small
+	arr[$index]=$temp
+done
+#printing sorted array
+echo ${arr[*]}
