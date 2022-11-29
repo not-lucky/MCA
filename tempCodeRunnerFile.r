f503422a5e7e277d6c224eef26291a4dbff1a663
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-11-30 04:04:53 +0530

    i am bored now

---
+++ tempCodeRunnerFile.r
@@ -0,0 +1,8 @@
+num = integer(readline(prompt="Enter a number: "))
+if(num > 0) {
+  print("Entered number is a positive number.")
+} else if (num < 0) {
+    print("Entered number is a negative number.")
+} else {
+    print("Entered number is 0.")
+}
