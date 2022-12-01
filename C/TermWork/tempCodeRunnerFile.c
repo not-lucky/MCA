1e04898020412dd3514fdcd2969f1e322e5d8197
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-12-01 18:33:08 +0530

    Last Sync: 2022-12-01 18:33:08

--- C/TermWork/tempCodeRunnerFile.c
+++ C/TermWork/tempCodeRunnerFile.c
@@ -1,2 +1,9 @@
-", 'A' + n - j);
-    for (int j = i - 1; j >= 1; j--) printf("%c ", 'A' + n -
+#include <stdio.h>
+
+int main() {
+  int x = 10000000;
+  int* y = &x;
+  int** z = &y;
+  int*** z_address = &z;
+  printf("x = %d, y = %d, z = %d , z address = %d", x, y, z, z_address);
+}
