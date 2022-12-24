87f0a84863071826fb66ca76f346da2fd5a62d59
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-12-24 18:31:31 +0530

    Continued development...

--- Scripting_lang/js/js_2.js
+++ Scripting_lang/js/js_2.js
@@ -5,20 +5,26 @@ let numChars = 0;
 let numVowels = 0;
 let numConsonants = 0;
 
-// Split the input string into an array of words
-const words = inputString.split(" ");
+// const words = inputString.split(" ");
 
-// Calculate the number of words and characters in the input string
-numWords = words.length;
-numChars = inputString.length;
+// numWords = words.length;
+// numChars = inputString.length;
 
-for (let i = 0; i < inputString.length; i++) {
-  let ch = inputString[i].toLowerCase();
+if (inputString) {
+  numWords = 1;
+  for (let i = 0; i < inputString.length; i++) {
+    let ch = inputString[i].toLowerCase();
 
-  if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-    numVowels++;
-  } else if (ch >= 'a' && ch <= 'z') {
-    numConsonants++;
+    if (ch == ' ') {
+      numWords++;
+    }
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+      numVowels++;
+    } else if (ch >= 'a' && ch <= 'z') {
+      numConsonants++;
+    }
+
+    numChars++;
   }
 }
 
