49baec69a10920bb8d5f53d211e3663b0359a5c8
not-lucky <57046712+not-lucky@users.noreply.github.com>
2023-01-14 06:52:54 +0530

    uhhhhhh

--- C/TermWork/2/tempCodeRunnerFile.c
+++ C/TermWork/2/tempCodeRunnerFile.c
@@ -1,1 +1,26 @@
-#include <conio.h>
+#include <stdio.h>
+
+int reverse(int);
+
+int main() {
+  int num = 12120, rev;
+
+  rev = reverse(num);
+
+  printf("The reverse of %d is %d\n", num, rev);
+
+  return 0;
+}
+
+int reverse(int num) {
+  static int rem, sum = 0;
+
+  if (num) {
+    rem = num % 10;
+    sum = sum * 10 + rem;
+    reverse(num / 10);
+  } else {
+    return 0;
+  }
+  return sum;
+}
