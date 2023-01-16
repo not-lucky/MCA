0df56b317ee0ee228c93be62c0b85dda276c288b
not-lucky <57046712+not-lucky@users.noreply.github.com>
2023-01-16 06:00:05 +0530

    This will definitely break in 2048 (TODO)

--- C/TermWork/2/tempCodeRunnerFile.c
+++ C/TermWork/2/tempCodeRunnerFile.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+int rever(int num) {
+  static int bin = 0;
+  if (num == 0) return bin;
+  bin = bin * 10 + num % 2;
+  return rever(num / 2);
+}
 
 int main(int argc, char *argv[]) {
-  FILE *fp;
-  char buffer[1000];
-  fp = fopen("S:/Emp.dat", "r");
-  fseek(fp, 0, SEEK_END);
-  long size = ftell(fp);
-  long i = 0;
-  fseek(fp, -1, SEEK_END);
-  printf("%c", fgetc(fp));
-  char ch;
-  while (i < size) {
-    if (ch == 10) fseek(fp, -3, SEEK_CUR);
-    else fseek(fp, -2, SEEK_CUR);
-    ch = fgetc(fp);
-    printf("%c", ch);
-    i++;
-  }
+  printf("%d", rever(21));
 }
